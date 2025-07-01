58c2b29d8cb50600917775f21e21ebde9ca3087b
AlejoSebas02 <alejobsc0902@gmail.com>
2025-07-01 01:01:54 -0500

    Update db.php

--- Models/db.php
+++ Models/db.php
@@ -1,16 +1,13 @@
 <?php
-$host = 'localhost';
-$port = 3306;
-$user = 'root';
-$pass = ''; 
-$dbname = 'cuarto';
-try{
+$host = getenv('DB_HOST') ?: 'metro.proxy.rlwy.net';
+$port = getenv('DB_PORT') ?: 18806;
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: 'YSXhNtkPSbhqwZsCyOktwOhFStWkGKbb';
+$dbname = getenv('DB_NAME') ?: 'railway';
+
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
-}catch(Exception $e){  
-    echo json_encode(['errorMsg' => 'Error al actualizar el usuario: ' . $e->getMessage()]);
-    exit;
-}
+
 if ($conn->connect_error) {
-    die('Error de conexión: ' . $conn->connect_error);
+    die(json_encode(['errorMsg' => 'Error de conexión: ' . $conn->connect_error]));
 }
 ?>
